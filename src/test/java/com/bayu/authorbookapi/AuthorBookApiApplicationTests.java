513aaa151172807b64dd0e989150a1ef25902d2f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-09-27 16:29:57 +0700

    Initial commit

---
+++ src/test/java/com/bayu/authorbookapi/AuthorBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.authorbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
