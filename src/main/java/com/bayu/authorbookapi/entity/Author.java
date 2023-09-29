4847da0474f7d7dcc8f70413f9a337f60b939255
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-09-29 09:19:46 +0700

    add entity Author

---
+++ src/main/java/com/bayu/authorbookapi/entity/Author.java
@@ -0,0 +1,27 @@
+package com.bayu.authorbookapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books = new ArrayList<>();
+}
